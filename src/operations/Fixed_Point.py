830533ea17b8caa876208e3d0951aebf5f7d029d
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-29 14:16:18 +0200

    fixed_point method

---
+++ src/operations/Fixed_Point.py
@@ -0,0 +1,25 @@
+import sympy as sp 
+
+def fixed_point(g, x0, rel_err=0.00001, maxiter=10, signif_digits=6):
+    flag, steps, roots = True, [], []
+    x = sp.symbols('x')
+    gx = sp.sympify(g)
+    for i in range(maxiter):
+        #calculate with signification digits
+        x1 = sp.N(gx.subs(x, x0), signif_digits)
+        roots.append(x1)
+        #explaine the steps
+        error = sp.N(abs((x1 - x0)/x1)*100, signif_digits)
+        step = f'Iteration {i+1}:   x{i+1} = {x1}   relative error = {error}\n'
+        steps.append(step)
+        if error < rel_err:
+            return True, steps, roots
+        x0 = x1
+    return False, steps, roots
+
+if __name__ == "__main__":
+    g = 'exp(-x)'
+    x0 = 0
+    flag, steps, roots = fixed_point(g, x0)
+    print(steps)
+    print(roots)
