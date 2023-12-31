0808cbfd668cb69e8575ba725799281252344a92
Ahmad Hassan <mi010203269@gmail.com>
2023-12-31 23:36:47 +0200

    add points

--- src/ui_logic/plotter_logic.py
+++ src/ui_logic/plotter_logic.py
@@ -10,8 +10,9 @@ from matplotlib import pyplot as plt
 
 class MplCanvas(FigureCanvasQTAgg):
 
-    def __init__(self, function, x_range=(-10, 10),parent=None, width=5, height=4, dpi=100):
+    def __init__(self, function,num_points=2, x_range=(-10, 10),parent=None, width=5, height=4, dpi=100):
         error,fig,self.axes=plot_function(function,x_range=x_range)
         super(MplCanvas, self).__init__(fig)
+        points=click_points(plt,function,num_points)
      
 
