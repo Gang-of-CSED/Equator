a22a6a9b171fd5571ab42ec5e67fcae6d3bd61e7
nasr7322 <nasr7322@gmail.com>
2024-01-01 00:05:02 +0200

    testing and error prevention

--- src/operations/NewtonRaphson.py
+++ src/operations/NewtonRaphson.py
@@ -19,10 +19,14 @@ def ModificationTwo(str_equation, x0, precision=5, eps=1e-5, max_iterations=50):
         f = equation.subs(x, x0)
         f1 = diff.subs(x, x0)
         if f1 == 0:
-            error = "First diff of equation is zero, cannot proceed"
-            return error, [], []
+            error = "First derivative of equation becomes zero at " + (f"it: {i+1}")
+            return error, roots, steps
+        
         f2 = diff2.subs(x, x0)
         root = x0 - (f * f1) / (f1 ** 2 - f * f2)
+        if roots.count(root) > 1:
+            error = "Method diverges at " + (f"it: {i}")
+            return error, [], []
         roots.append(root)
         if abs(root - x0) < eps:
             break
@@ -50,9 +54,12 @@ def ModificationOne(str_equation, x0, m=1, precision=5, eps=1e-5, max_iterations
         f = equation.subs(x, x0)
         f1 = diff.subs(x, x0)
         if f1 == 0:
-            error = "First diff of equation is zero, cannot proceed"
-            return error, [], []
+            error = "First derivative of equation becomes zero at " + (f"it: {i+1}")
+            return error, roots, steps
         root = x0 - m * (f / f1)
+        if roots.count(root) > 1:
+            error = "Method diverges at " + (f"it: {i}")
+            return error, [], []
         roots.append(root)
         if abs(root - x0) < eps:
             break
@@ -65,27 +72,32 @@ def ModificationOne(str_equation, x0, m=1, precision=5, eps=1e-5, max_iterations
 ## main function
 if __name__ == '__main__':
     
-    str_equation = "x**5 - 11*x**4 + 46*x**3 - 90*x**2 + 81*x - 27"
-    x0 = 5
-    m = 3
-    precision = 2
-    eps = 1e-6
-    max_iterations = 200
-    
-    print(str_equation)
-    
-    #print true root of the function
-    x = sp.symbols('x')
-    equation = sp.N(sp.sympify(str_equation), precision)
-    root = sp.solve(equation, x)
-    print(f"\nThe true roots of the equation is {root}\n\n")
-   
-    print("Modification One\n")
-    error1 ,roots1, steps1 = ModificationOne(str_equation, x0, m, precision, eps, max_iterations)
-    print(f"The root of the equation is approximately {roots1[-1]}\n")
-    print(error1, roots1, steps1)
-    
-    print("Modification Two\n")
-    error2, roots2, steps2 = ModificationTwo(str_equation, x0, precision, eps, max_iterations)
-    print(f"The root of the equation is approximately {roots2[-1]}\n")
-    print(error2, roots2, steps2)
+    test_cases = [
+        ("x**3 - 2*x**2 - 5", 5, 1, 4, 1e-6, 200),  # root multiplicity: 1
+        ("x**3 - x**2 + 2", -20, 1, 5, 1e-6, 200),  # root multiplicity: 1
+        ("x**4 - 3*x**3 + 2", 1, 1, 6, 1e-6, 200),  # root multiplicity: 1
+        ("x**5 - 11*x**4 + 46*x**3 - 90*x**2 + 81*x - 27", 5, 1, 4, 1e-6, 200),  # root multiplicity: 1
+        ("x**2 - 4", 5, 2, 5, 1e-6, 200),  # root multiplicity: 2
+        ("x**3 - 2*x - 5", 5, 1, 6, 1e-6, 200),  # root multiplicity: 1
+        ("x**3 - 3*x**2 + 2*x - 1", 5, 1, 4, 1e-6, 200),  # root multiplicity: 1
+        ("x**4 - 4*x**3 + 6*x**2 - 4*x + 1", 5, 1, 5, 1e-6, 200),  # root multiplicity: 1
+        ("x**5 - 5*x**4 + 10*x**3 - 10*x**2 + 5*x - 1", 5, 1, 6, 1e-6, 200),  # root multiplicity: 1
+        ("x**6 - 6*x**5 + 15*x**4 - 20*x**3 + 15*x**2 - 6*x + 1", 5, 1, 4, 1e-6, 200),  # root multiplicity: 1
+        ("x + 2", 5, 1, 4, 1e-6, 200),  # root multiplicity: 1
+        ("10", 5, 0, 4, 1e-6, 200),  # root multiplicity: 0 (constant function)
+    ]
+
+    for i, (str_equation, x0, m, precision, eps, max_iterations) in enumerate(test_cases, 1):
+        x0 = 15
+        print(f"Test case {i}: {str_equation}")
+        x = sp.symbols('x')
+        equation = sp.N(sp.sympify(str_equation), precision)
+        root = sp.solve(equation, x)
+        print(f"\nThe true roots of the equation is {root}\n")
+        
+        # error1, roots1, steps1 = ModificationOne(str_equation, x0, m, precision, eps, max_iterations)
+        # print("modification one", error1 ,"\n", roots1 ,"\n" , steps1 ,"\n")
+
+        error2, roots2, steps2 = ModificationTwo(str_equation, x0, precision, eps, max_iterations)
+        print("modification two", error2 ,"\n", roots2 ,"\n" , steps2 ,"\n\n\n")
+       
