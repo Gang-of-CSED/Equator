598f490020442b930804419059e4da689b119c7e
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-31 20:51:44 +0200

    fix relative err

--- src/operations/Fixed_Point.py
+++ src/operations/Fixed_Point.py
@@ -1,5 +1,20 @@
 import sympy as sp 
 
+
+def relative_error(x, x0):
+    """
+    :return: relative error
+    """
+    if x == None:
+        return 1e10
+    if x == x0:
+        return 0
+    if x0 == 0:
+        return 1e10
+    return abs(x - x0) / abs(x)
+
+
+
 def fixed_point(g, x0, rel_err=0.00001, maxiter=10, signif_digits=6):
     flag, steps, roots = True, [], []
     x = sp.symbols('x')
@@ -9,7 +24,7 @@ def fixed_point(g, x0, rel_err=0.00001, maxiter=10, signif_digits=6):
         x1 = sp.N(gx.subs(x, x0), signif_digits)
         roots.append(x1)
         #explaine the steps
-        error = sp.N(abs((x1 - x0)/x1)*100, signif_digits)
+        error = sp.N(relative_error(x, x0), signif_digits)
         step = f'Iteration {i+1}:   x{i+1} = {x1}   relative error = {error}\n'
         steps.append(step)
         if error < rel_err:
