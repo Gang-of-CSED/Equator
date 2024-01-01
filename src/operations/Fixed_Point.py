e21b5db9d2030abcc52116e72d17e80c24c74b4d
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-01-01 15:13:04 +0200

    .

--- src/operations/Fixed_Point.py
+++ src/operations/Fixed_Point.py
@@ -17,18 +17,18 @@ def fixed_point(g, x0, rel_err=0.00001, maxiter=10, signif_digits=6):
             step = f'Iteration {i+1}:   x{i+1} = {x1}   relative error = {error}\n'
             steps.append(step)
             if error < rel_err:
-                return True, 'Solution Found', steps, roots
+                return True, 'Solution found and method has converged', steps, roots
             x0 = x1
         if error > errors[-2]:
-            return True, 'Convergent', steps, roots
+            return True, 'Method diverges', steps, roots
         else:
-            return False, 'Divergent', steps, roots
+            return True, 'Method converges', steps, roots
     except Exception as e:
         return False, e, steps, roots
 
 if __name__ == "__main__":
     g = 'exp(-x)'
     x0 = 0
-    flag, steps, roots = fixed_point(g, x0)
+    flag, message, steps, roots = fixed_point(g, x0)
     print(steps)
     print(roots)
