b30cf3656f0bf330c04c3f3c0f67db6dc2a704b0
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2024-01-01 15:08:05 +0200

    .

--- src/operations/Fixed_Point.py
+++ src/operations/Fixed_Point.py
@@ -2,21 +2,29 @@ import sympy as sp
 from utils import relative_error
 
 def fixed_point(g, x0, rel_err=0.00001, maxiter=10, signif_digits=6):
-    flag, steps, roots = True, [], []
-    x = sp.symbols('x')
-    gx = sp.sympify(g)
-    for i in range(maxiter):
-        #calculate with signification digits
-        x1 = sp.N(gx.subs(x, x0), signif_digits)
-        roots.append(x1)
-        #explaine the steps
-        error = relative_error(x1, x0)
-        step = f'Iteration {i+1}:   x{i+1} = {x1}   relative error = {error}\n'
-        steps.append(step)
-        if error < rel_err:
-            return True, steps, roots
-        x0 = x1
-    return False, steps, roots
+    try:
+        flag, converge, steps, roots = True, '', [], []
+        errors = []
+        x = sp.symbols('x')
+        gx = sp.sympify(g)
+        for i in range(maxiter):
+            #calculate with signification digits
+            x1 = sp.N(gx.subs(x, x0), signif_digits)
+            roots.append(x1)
+            #explaine the steps
+            error = relative_error(x1, x0)
+            errors.append(error)
+            step = f'Iteration {i+1}:   x{i+1} = {x1}   relative error = {error}\n'
+            steps.append(step)
+            if error < rel_err:
+                return True, 'Solution Found', steps, roots
+            x0 = x1
+        if error > errors[-2]:
+            return True, 'Convergent', steps, roots
+        else:
+            return False, 'Divergent', steps, roots
+    except Exception as e:
+        return False, e, steps, roots
 
 if __name__ == "__main__":
     g = 'exp(-x)'
